2760601d098aaeb51eaf68a3027073f145b18636
manutencaobandeirantesdeicmar-boop <manutencaobandeirantesdeicmar@gmail.com>
2026-06-08 10:48:13 -0300

    Update supabase-config.js

--- src/services/supabase-config.js
+++ src/services/supabase-config.js
@@ -1,6 +1,9 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-const supabaseUrl = 'https://jwugagaspfputbphtjkm.supabase.co'
-const supabaseKey = 'sb_publishable_Ct9ytY359hGWZIytnLmcSw_Lnh5Khgz'
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Variáveis de ambiente do Supabase não encontradas! Verifique o seu ficheiro .env");
+}
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey);
