ea3b04dfd66ae9d5660dcd3925fcd73038ed9941
manutencaobandeirantesdeicmar-boop <manutencaobandeirantesdeicmar@gmail.com>
2026-06-08 11:35:54 -0300

    Update supabaseFrota-config.js

--- src/services/supabaseFrota-config.js
+++ src/services/supabaseFrota-config.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-// URL corrigida (sem o /rest/v1/ no final)
-const supabaseUrl = 'https://dwlcaplumgtgvbducrev.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3bGNhcGx1bWd0Z3ZiZHVjcmV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY4MzY5NDMsImV4cCI6MjA2MjQxMjk0M30.8oGZIvEIruVdOjuMT-oPtgOGLh_QgfR3XV07V3AOe40';
+// Busca as variáveis específicas da frota
+const supabaseUrl = import.meta.env.VITE_SUPABASE_FROTA_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_FROTA_ANON_KEY;
 
 // Exportando com o nome exato que a tela da Frota está procurando
 export const supabaseFrota = createClient(supabaseUrl, supabaseKey);
