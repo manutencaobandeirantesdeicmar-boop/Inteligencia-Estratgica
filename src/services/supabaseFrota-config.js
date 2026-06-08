9f80f00489eb51c5042afeb45951cff2052c12ed
manutencaobandeirantesdeicmar-boop <manutencaobandeirantesdeicmar@gmail.com>
2026-06-08 11:31:36 -0300

    Update supabaseFrota-config.js

--- src/services/supabaseFrota-config.js
+++ src/services/supabaseFrota-config.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL2;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY2;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL_2;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY_2;
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Variáveis de ambiente do Supabase não encontradas! Verifique o seu ficheiro .env");
